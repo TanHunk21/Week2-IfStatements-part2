0971f5dc3517104350dd6857da60ae7e743a6d78
Opilane TTHK <opilane@tthk.ee>
2020-09-08 14:22:03 +0300

    Week2IfStatements

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Age;
+            string UserInput;
+
+            Console.WriteLine("How old are you?");
+            UserInput = Console.ReadLine();
+            Age = Int32.Parse(UserInput);
+
+            if (Age < 18)
+            {
+                Console.WriteLine($"you are {Age}, you are too young to get a drivers license.");
+            }
+            else if (Age > 18)
+            {
+                Console.WriteLine($"you are {Age}, you are old enough to get a driving license.");
+            }
+            else
+            {
+               
+            }
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Age;
+            string UserInput;
+
+            Console.WriteLine("Choose a number between 1-100 ?");
+            UserInput = Console.ReadLine();
+            Age = Int32.Parse(UserInput);
+
+            if (Age < 50)
+            {
+                Console.WriteLine($"{Age} < 50");
+            }
+            else if (Age > 50)
+            {
+                Console.WriteLine($"{Age} > 50");
+            }
+            else
+            {
+                Console.WriteLine($"Nice {Age}");
+            }else
+
+
+
+
+
+
+        }
+    }
+}
